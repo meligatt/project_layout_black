286c64f9f3a71d2837746111f291a2ee2165fc3b
melissa gattoni <meligatt@gmail.com>
2017-03-09 13:51:39 +1100

    add modal styles, add modal scripts

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,6 @@
 // js
+const openModal = document.querySelector(`.button--open-modal`);
+openModal.addEventListener('click', (event) => {
+
+  console.log("clicked on a button--open-modal", event.target);
+});
